24b439c7494410884b13c13369926cec1a7f8c2b
dovilemat <95312847+dovilemat@users.noreply.github.com>
2021-12-14 16:12:56 +0000

    Update contract.py

--- contract.py
+++ contract.py
@@ -85,7 +85,7 @@ print("Output from greet()")
 print(greeter.functions.greet().call())
 
 nonce = W3.eth.getTransactionCount(address1)
-tx_dict = greeter.functions.setGreeting('Nihao').buildTransaction({
+tx_dict = greeter.functions.setGreeting('Hello or Labas from Dovile Matelyte, student ID 10380791').buildTransaction({
   'chainId': 3,
   'gas': 1400000,
   'gasPrice': w3.toWei('40', 'gwei'),
